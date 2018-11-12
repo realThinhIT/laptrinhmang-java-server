2023cb9f0a09fea8e7e394f177c27da607359b41
nvs266 <nvs266@gmail.com>
2018-11-13 03:16:50 +0700

    Add model

---
+++ src/models/Room.java
@@ -0,0 +1,81 @@
+package models;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Room implements Serializable {
+
+    private long mId;
+    private String mName;
+    private User mOwner;
+    private String mCreatedAt;
+    private long mStatus;
+    private List<RoomMessage> mRoomMessages;
+    private List<RoomUser> mRoomUsers;
+
+    public Room(long id, String name, User owner, String createdAt, long status, List<RoomMessage> roomMessages, List<RoomUser> roomUsers) {
+        mId = id;
+        mName = name;
+        mOwner = owner;
+        mCreatedAt = createdAt;
+        mStatus = status;
+        mRoomMessages = roomMessages;
+        mRoomUsers = roomUsers;
+    }
+
+    public long getId() {
+        return mId;
+    }
+
+    public void setId(long id) {
+        mId = id;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public User getOwner() {
+        return mOwner;
+    }
+
+    public void setOwner(User owner) {
+        mOwner = owner;
+    }
+
+    public String getCreatedAt() {
+        return mCreatedAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        mCreatedAt = createdAt;
+    }
+
+    public long getStatus() {
+        return mStatus;
+    }
+
+    public void setStatus(long status) {
+        mStatus = status;
+    }
+
+    public List<RoomMessage> getRoomMessages() {
+        return mRoomMessages;
+    }
+
+    public void setRoomMessages(List<RoomMessage> roomMessages) {
+        mRoomMessages = roomMessages;
+    }
+
+    public List<RoomUser> getRoomUsers() {
+        return mRoomUsers;
+    }
+
+    public void setRoomUsers(List<RoomUser> roomUsers) {
+        mRoomUsers = roomUsers;
+    }
+}
---
+++ src/models/RoomMessage.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.io.Serializable;
+
+public class RoomMessage implements Serializable {
+
+    private long mId;
+    private String mContent;
+    private long mUserId;
+    private long mRoomId;
+    private String mCreateAt;
+
+    public RoomMessage(long id, String content, long userId, long roomId, String createAt) {
+        mId = id;
+        mContent = content;
+        mUserId = userId;
+        mRoomId = roomId;
+        mCreateAt = createAt;
+    }
+
+    public long getId() {
+        return mId;
+    }
+
+    public void setId(long id) {
+        mId = id;
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String content) {
+        mContent = content;
+    }
+
+    public long getUserId() {
+        return mUserId;
+    }
+
+    public void setUserId(long userId) {
+        mUserId = userId;
+    }
+
+    public long getRoomId() {
+        return mRoomId;
+    }
+
+    public void setRoomId(long roomId) {
+        mRoomId = roomId;
+    }
+
+    public String getCreateAt() {
+        return mCreateAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        mCreateAt = createAt;
+    }
+}
