ccf584af0665f0f4839a6acfeb3c5f3a485f7ff9
nvs266 <nvs266@gmail.com>
2018-11-13 16:35:53 +0700

    add server control

---
+++ src/controllers/Server.java
@@ -0,0 +1,51 @@
+package controllers;
+
+import models.User;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+public class Server {
+    private ServerSocket mServerSocket;
+    private HashMap<String, User> mThreadNameUserHashMap = new HashMap<>();
+
+    public void openServer() {
+        try {
+            mServerSocket = new ServerSocket(9999);
+        } catch (IOException e) {
+            System.out.println("------- Server Error -------\n" + e.getMessage() + "\n---------------------");
+        }
+    }
+
+    public void listening() {
+        while (true) {
+            try {
+                Socket clientSocket = mServerSocket.accept();
+
+                ClientThread clientThread = new ClientThread(clientSocket, this);
+                addClientSocket(clientSocket);
+                clientThread.run();
+            } catch (IOException e) {
+                System.out.println("------- Client Error -------\n" + e.getMessage() + "\n---------------------");
+            }
+        }
+    }
+
+    public void close() {
+        try {
+            mServerSocket.close();
+        } catch (IOException e) {
+            System.out.println("------- Server Error -------\n" + e.getMessage() + "\n---------------------");
+        }
+    }
+
+    public void removeClientSocket(String remoteSocketAddress) {
+        mThreadNameUserHashMap.remove(remoteSocketAddress);
+    }
+
+    private void addClientSocket(Socket clientSocket) {
+        mThreadNameUserHashMap.put(clientSocket.getRemoteSocketAddress().toString(), null);
+    }
+}
